d518e17b5793909cb7fde67092ed75e76d21c0bd
MateoMerja <merjamateogit@gmail.com>
2025-09-04 00:14:48 +0200

    Add files via upload
    Online shop for phones computers playstations

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './Page.css';
+import NavigationBar from './NavigationBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar/>
+    </div>
+  );
+}
+
+export default App;
